b25a5c2b2f0a66cbfae78803155f8c3b2467413a
alisilakg <wisper14@yandex.ru>
2023-04-07 20:54:54 +0500

    10 спринт вариант 4

---
+++ src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
@@ -0,0 +1,74 @@
+package ru.yandex.practicum.filmorate;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.controller.FilmController;
+import ru.yandex.practicum.filmorate.exceptions.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.FilmService;
+import ru.yandex.practicum.filmorate.storage.InMemoryFilmStorage;
+import ru.yandex.practicum.filmorate.storage.InMemoryUserStorage;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class FilmControllerTest {
+    FilmController filmController;
+    Film film;
+    @BeforeEach
+    void beforeEach() {
+        filmController = new FilmController(new FilmService(new InMemoryFilmStorage(new InMemoryUserStorage())));
+        film = new Film();
+        film.setName("Film");
+        film.setDescription("Very interesting film");
+        film.setReleaseDate(LocalDate.now());
+        film.setDuration(30);
+    }
+
+    @Test
+    void shouldPostFilmWithEmptyName() {
+        film.setName(" ");
+        final ValidationException exception = assertThrows(
+                ValidationException.class,
+                () -> filmController.create(film));
+        assertEquals("Название фильма не может быть пустым.", exception.getMessage());
+    }
+
+    @Test
+    void shouldPostFilmWithDescriptionLonger200() {
+        StringBuilder longDescription = new StringBuilder("Very ");
+        for(int i = 1; i < 200; i++) {
+            String newString = "very ";
+            longDescription.append(newString);
+        }
+        longDescription.append("long description.");
+        film.setDescription(longDescription.toString());
+        final ValidationException exception = assertThrows(
+                ValidationException.class,
+                () -> filmController.create(film));
+        assertEquals("Описание фильма должно быть меньше 200 символов.", exception.getMessage());
+    }
+
+    @Test
+    void shouldPostFilmWithReleaseDateEarlierThanBirthDayMovie() {
+        film.setReleaseDate(LocalDate.of(1895, 12, 27));
+        final ValidationException exception = assertThrows(
+                ValidationException.class,
+                () -> filmController.create(film));
+        assertEquals("Дата релиза не может быть раньше 28.12.1895.", exception.getMessage());
+    }
+
+    @Test
+    void shouldPostFilmWithNegativeDuration() {
+        film.setDuration(-30);
+        final ValidationException exception = assertThrows(
+                ValidationException.class,
+                () -> filmController.create(film));
+        assertEquals("Продолжительность фильма не должна быть отрицательной.", exception.getMessage());
+    }
+
+}
+
---
+++ src/test/java/ru/yandex/practicum/filmorate/UserControllerTest.java
@@ -0,0 +1,89 @@
+package ru.yandex.practicum.filmorate;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.controller.UserController;
+import ru.yandex.practicum.filmorate.exceptions.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.UserService;
+import ru.yandex.practicum.filmorate.storage.InMemoryUserStorage;
+
+import java.time.LocalDate;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+public class UserControllerTest {
+    UserController userController;
+
+    User user;
+    @BeforeEach
+    void beforeEach() {
+        userController = new UserController(new UserService(new InMemoryUserStorage()));
+        user = new User();
+        user.setEmail("email@email.ru");
+        user.setLogin("Login");
+        user.setName("Name");
+        user.setBirthday(LocalDate.of(2000, 1,1));
+
+    }
+
+    @Test
+    void shouldPostUserWithEmptyEmail() {
+        user.setEmail(" ");
+        final ValidationException exception = assertThrows(
+                ValidationException.class,
+                () -> userController.create(user));
+        assertEquals("Адрес электронной почты не может быть пустым.", exception.getMessage());
+    }
+
+    @Test
+    void shouldPostUserWithEmailWithoutSpecialSymbol() {
+        user.setEmail("email.ru");
+        final ValidationException exception = assertThrows(
+                ValidationException.class,
+                () -> userController.create(user));
+        assertEquals("Адрес электронной почты должен содержать символ @.", exception.getMessage());
+    }
+
+    @Test
+    void shouldPostUserWithEmptyLogin() {
+        user.setLogin(" ");
+        final ValidationException exception = assertThrows(
+                ValidationException.class,
+                () -> userController.create(user));
+        assertEquals("Логин не может быть пустым.", exception.getMessage());
+    }
+
+    @Test
+    void shouldPostUserWithLoginWithSpase() {
+        user.setLogin("Lo gin");
+        final ValidationException exception = assertThrows(
+                ValidationException.class,
+                () -> userController.create(user));
+        assertEquals("Логин не может содержать пробелы.", exception.getMessage());
+    }
+
+    @Test
+    void shouldPostUserWithNameEmptyAndGetNameEqualsLogin() {
+        user.setName("");
+        userController.create(user);
+
+        List<User> users = userController.findAll();
+        User actualUser = users.get(0);
+
+        assertEquals("Login", actualUser.getName());
+    }
+
+    @Test
+    void shouldPostUserWithBirthdayInFuture() {
+        user.setBirthday(LocalDate.now().plusDays(1));
+        final ValidationException exception = assertThrows(
+                ValidationException.class,
+                () -> userController.create(user));
+        assertEquals("Дата рождения не может быть в будущем.", exception.getMessage());
+    }
+}
