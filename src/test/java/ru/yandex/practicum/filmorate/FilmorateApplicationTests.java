bddc22cd053abf2dd942f8d7fbdd46cdc3c7f92d
alisilakg <wisper14@yandex.ru>
2023-05-12 05:37:06 +0500

    6

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,21 +1,10 @@
 package ru.yandex.practicum.filmorate;
 
 import lombok.RequiredArgsConstructor;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.context.TestConstructor;
-import ru.yandex.practicum.filmorate.dao.FilmDbStorage;
-import ru.yandex.practicum.filmorate.dao.UserDbStorage;
-import ru.yandex.practicum.filmorate.model.User;
 
-import java.time.LocalDate;
-import java.util.Optional;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 @SpringBootTest
@@ -23,33 +12,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 class FilmorateApplicationTests {
 
-    private final UserDbStorage userDbStorage;
-    private final FilmDbStorage filmDbStorage;
-
-    @Test
-    public void testFindUserById() {
-
-        Optional<User> userOptional = Optional.ofNullable(userDbStorage.findUserById(1));
-
-        assertThat(userOptional)
-                .isPresent()
-                .hasValueSatisfying(user ->
-                        assertThat(user).hasFieldOrPropertyWithValue("id", 1)
-                );
-    }
-    @Test
-    public void getUserById_shouldConfirmThatUserNameExists() {
-        User user = new User();
-
-        user.setName("Aleksandr");
-        user.setLogin("sashajaaa");
-        user.setEmail("sashajaaa@yandex.ru");
-        user.setBirthday(LocalDate.now().minusYears(35));
-        userDbStorage.addUser(user);
-        User userOptional = userDbStorage.findUserById(1);
-
-        assertEquals(userOptional.getName(), "Aleksandr");
-    }
 }
 
 
