dd0ec2ee1b650338649e9bcc7d8f979dbb8c6c38
Iwavvnc1 <popigbang@yandex.ru>
2023-05-27 01:36:58 +0500

    new feature:add-common-films refactor FilmDbStorage.java

--- src/main/java/ru/yandex/practicum/filmorate/storage/CommonFilmsStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/CommonFilmsStorage.java
@@ -5,5 +5,5 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.util.List;
 
 public interface CommonFilmsStorage {
-    List<Film> getCommonFilms(Long userId, Long friendId );
+    List<Film> getCommonFilms(Long userId, Long friendId);
 }
