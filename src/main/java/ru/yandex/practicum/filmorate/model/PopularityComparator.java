d3a5f67e080238d18bf7ff0955dc5e619c25f738
Flykeeper <flykeeper@yandex.ru>
2023-05-28 03:41:16 +0500

    fix: codestyle

--- src/main/java/ru/yandex/practicum/filmorate/model/PopularityComparator.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/PopularityComparator.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 public class PopularityComparator implements Comparator<Film> {
     @Override
     public int compare(Film o1, Film o2) {
-/* Если положительный результат => у второго фильма больше лайков
+        /* Если положительный результат => у второго фильма больше лайков
         Если ноль => равное количество лайков
         Если отрицательный результат => у второго фильма меньше лайков */
         return Integer.compare(o2.getLikes().size(), o1.getLikes().size());
