be3e95e4964042b7932d9a4aa1346d05e0c789dd
flykeeperB <117763466+flykeeperB@users.noreply.github.com>
2023-06-01 10:57:23 +0500

    Update StorageException.java
    codestyle

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/StorageException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/StorageException.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.exceptions;
 
 import lombok.extern.slf4j.Slf4j;
+
 @Slf4j
 public class StorageException extends RuntimeException {
     public StorageException(String message) {
